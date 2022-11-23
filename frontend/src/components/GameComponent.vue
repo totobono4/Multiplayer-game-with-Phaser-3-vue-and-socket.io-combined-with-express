5471870bca92b22e07cf3f5f8460fa1ef7b30599
Belzorium <florian.boyer@imerir.com>
2022-11-23 09:19:34 +0100

    added level register

--- frontend/src/components/GameComponent.vue
+++ frontend/src/components/GameComponent.vue
@@ -4,12 +4,14 @@
 
 <script setup>
 import Game from '../scripts/game/game.ts'
+import Level1 from '../scripts/game/levels/Level1'
 import { onMounted } from 'vue'
 const containerId = 'game-container'
 
 onMounted(() => {
-  new Game(800, 800)
+  new Game('100%', '100%')
     .attachTo(containerId)
+    .registerLevel(Level1, true)
     .start()
 })
 
